b5ea0e016822d6fce88a704c0d00518326cd1612
MatInGit <mateo.leputa@googlemail.com>
2024-03-25 14:47:17 -0700

    [FIX] added missing module files

---
+++ flow_prototype/my_flow/flow_utils/mlflow_flow.py
@@ -0,0 +1,36 @@
+from prefect import flow, get_run_logger, tags, task
+from mlflow import MlflowClient
+
+
+@task
+def get_models():
+    # list all registered models
+    logger = get_run_logger()
+    client = MlflowClient()  #
+    models = client.search_registered_models()
+    for model in models:
+        logger.info(f"Found model: {model.name}")
+
+    return models
+
+
+@task
+def get_by_model_aliases(models):
+    client = MlflowClient()
+    logger = get_run_logger()
+    model_info_dicts = []
+    for model in models:
+
+        model_info = client.get_model_version_by_alias(model.name, "champion")
+        logger.info(f"Model: {model.name}")
+
+        combined_dict = {**model.tags}
+        combined_dict["workflow_model_name"] = model.name
+        combined_dict["workflow_model_version"] = model_info.version
+        combined_dict["maintainer"] = None
+        combined_dict["deplyment_status"] = None
+        combined_dict["deployment_handler_type"] = "MLflow:pyfunc"
+
+        model_info_dicts.append(combined_dict)
+
+    return model_info_dicts
