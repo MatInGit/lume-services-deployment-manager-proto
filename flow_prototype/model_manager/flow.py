8bb1b1d3aea2452333eec2ea0c28c675933f9eb5
MatInGit <mateo.leputa@googlemail.com>
2024-04-11 11:25:29 -0700

    [ADD] pv_mapping files

--- flow_prototype/model_manager/flow.py
+++ flow_prototype/model_manager/flow.py
@@ -2,12 +2,17 @@ from prefect import flow, get_run_logger, tags, task
 import random
 from prefect.blocks.kubernetes import KubernetesClusterConfig
 from prefect_kubernetes.credentials import KubernetesCredentials
-from prefect_kubernetes.pods import list_namespaced_pod
-from prefect_kubernetes.pods import create_namespaced_pod
+from prefect_kubernetes.pods import (
+    list_namespaced_pod,
+    create_namespaced_pod,
+    delete_namespaced_pod,
+    read_namespaced_pod,
+)
 from kubernetes.client.models import V1Pod
 import uuid
 import os
 import hashlib
+import time
 
 from model_utils.workflow_base import BaseWorkflow  # base class for workflow
 from flow_utils.mlflow_flow import (
@@ -36,7 +41,7 @@ def process_model(workflow_instance: BaseWorkflow):
             "deployment_status",
             "starting_deployment",
         )
-        
+
         return workflow_instance
 
     elif workflow_instance.deployment_type == "batch":
@@ -53,6 +58,8 @@ def process_model(workflow_instance: BaseWorkflow):
             f"Invalid deployment type {workflow_instance.deployment_type} for model {workflow_instance.workflow_model_name}"
         )
         return None
+
+
 ###{
 #     "MLFLOW_TRACKING_USERNAME": "admin",
 #     "MLFLOW_TRACKING_PASSWORD": "password",
@@ -64,22 +71,31 @@ def process_model(workflow_instance: BaseWorkflow):
 #     "MLFLOW_S3_ENDPOINT_URL": "http://172.24.5.222:9020",
 #     "MLFLOW_TRACKING_URI": "https://ard-mlflow.slac.stanford.edu"
 #   }
-  
+
 
 @task
 def workflow_to_pod(workflow: BaseWorkflow):
 
-    
     workflow_hash = hashlib.md5(f"{str(workflow.dict())}".encode()).hexdigest()
     logger = get_run_logger()
-    logger.info(f"Creating pod for {workflow.workflow_model_name} with hash {workflow_hash}")
-    
+    logger.info(
+        f"Creating pod for {workflow.workflow_model_name} with hash {workflow_hash}"
+    )
+    unique_id = str(uuid.uuid4())[0:7]
     pod = V1Pod(
-        metadata={"name": f"{workflow.workflow_model_name}-{workflow.workflow_model_version}-{workflow_hash}", "namespace": "mlflow", "labels":{"md5chk": workflow_hash}},
+        metadata={
+            "name": f"{workflow.workflow_model_name}-{workflow.workflow_model_version}-{unique_id}".replace("_", "-"),
+            "namespace": "mlflow",
+            "labels": {
+                "md5chk": workflow_hash,
+                "model_name": workflow.workflow_model_name,
+                "model_version": workflow.workflow_model_version,
+            },
+        },
         spec={
             "containers": [
                 {
-                    "name": f"{workflow.workflow_model_name}-{workflow.workflow_model_version}",
+                    "name": f"{workflow.workflow_model_name}-{workflow.workflow_model_version}".replace("_", "-"),
                     "image": "matindocker/lumeservicesdeployment:latest",
                     "env": [
                         {"name": "model_name", "value": workflow.workflow_model_name},
@@ -87,31 +103,76 @@ def workflow_to_pod(workflow: BaseWorkflow):
                             "name": "model_version",
                             "value": workflow.workflow_model_version,
                         },
-                        {"name": "MLFLOW_TRACKING_URI", "value": os.environ.get("MLFLOW_TRACKING_URI")},
-                        {"name": "MLFLOW_TRACKING_USERNAME", "value": os.environ.get("MLFLOW_TRACKING_USERNAME")},
-                        {"name": "MLFLOW_TRACKING_PASSWORD", "value": os.environ.get("MLFLOW_TRACKING_PASSWORD")},
-                        {"name": "MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING", "value": os.environ.get("MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING")},
-                        {"name": "AWS_DEFAULT_REGION", "value": os.environ.get("AWS_DEFAULT_REGION")},
+                        {
+                            "name": "MLFLOW_TRACKING_URI",
+                            "value": os.environ.get("MLFLOW_TRACKING_URI"),
+                        },
+                        {
+                            "name": "MLFLOW_TRACKING_USERNAME",
+                            "value": os.environ.get("MLFLOW_TRACKING_USERNAME"),
+                        },
+                        {
+                            "name": "MLFLOW_TRACKING_PASSWORD",
+                            "value": os.environ.get("MLFLOW_TRACKING_PASSWORD"),
+                        },
+                        {
+                            "name": "MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING",
+                            "value": os.environ.get(
+                                "MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING"
+                            ),
+                        },
+                        {
+                            "name": "AWS_DEFAULT_REGION",
+                            "value": os.environ.get("AWS_DEFAULT_REGION"),
+                        },
                         {"name": "AWS_REGION", "value": os.environ.get("AWS_REGION")},
-                        {"name": "AWS_ACCESS_KEY_ID", "value": os.environ.get("AWS_ACCESS_KEY_ID")},
-                        {"name": "AWS_SECRET_ACCESS_KEY", "value": os.environ.get("AWS_SECRET_ACCESS_KEY")},
-                        {"name": "MLFLOW_S3_ENDPOINT_URL", "value": os.environ.get("MLFLOW_S3_ENDPOINT_URL")},
-                        
+                        {
+                            "name": "AWS_ACCESS_KEY_ID",
+                            "value": os.environ.get("AWS_ACCESS_KEY_ID"),
+                        },
+                        {
+                            "name": "AWS_SECRET_ACCESS_KEY",
+                            "value": os.environ.get("AWS_SECRET_ACCESS_KEY"),
+                        },
+                        {
+                            "name": "MLFLOW_S3_ENDPOINT_URL",
+                            "value": os.environ.get("MLFLOW_S3_ENDPOINT_URL"),
+                        },
                     ],
                 }
             ]
         },
     )
     return pod, workflow_hash
+
+
 @task
-def check_if_model_already_exists(namespaced_model_list,chcksum):
+def check_if_model_already_exists(
+    namespaced_model_list, model_name: str, model_version: str
+):
     logger = get_run_logger()
-    logger.info(f"Checking if pod {namespaced_model_list.items} already exists")
+    # logger.info(f"Checking if pod {namespaced_model_list.items} already exists")
     for pod in namespaced_model_list.items:
-        if pod.metadata.labels.get("md5chk") == chcksum:
-            logger.error(f"Pod {pod.metadata.name} already exists")
-            return True,pod.metadata.name
-    return False, None
+        # check name and version
+        logger.info(f"Checking pod labels: {pod.metadata.labels} ...")
+        if (
+            pod.metadata.labels.get("model_name") == model_name
+            and pod.metadata.labels.get("model_version") == model_version
+        ):
+            logger.info(
+                f"Pod {model_name} already exists with same version {model_version}"
+            )
+            return True, True, pod
+        # name same but version different
+        elif (
+            pod.metadata.labels.get("model_name") == model_name
+            and pod.metadata.labels.get("model_version") != model_version
+        ):
+            logger.info(f"Pod {model_name} exists but version is different")
+            return True, False, pod
+
+    return False, False, None
+
 
 @flow
 def model_manager():
@@ -129,13 +190,13 @@ def model_manager():
             workflow_instances.append(workflow_instance)
         except Exception as e:
             logger.warning(f"Error: {e} for model {model_dict['workflow_model_name']}")
+            
+    results = []
 
-    __futures = []
     for workflow_instance in workflow_instances:
         try:
             logger.info(f"Processing model {workflow_instance.workflow_model_name}")
-            result = process_model.submit(workflow_instance)
-            __futures.append(result)
+            results.append(process_model(workflow_instance))
         except Exception as e:
             logger.warning(
                 f"Error: {e} for model {workflow_instance.workflow_model_name}"
@@ -143,34 +204,80 @@ def model_manager():
 
     logger.info(f"Waiting for model pre-processing to complete")
 
-    results = []
-    while len(__futures) > len(results):
-        for future in __futures:
-            result = future.wait(0.1)
-            # need to consider if this is try/except
-            if result:
-                results.append(result.result())
-    
     namespaced_pod_list = list_namespaced_pod(
-            namespace="prefect",
-            kubernetes_credentials=kubernetes_credentials,
-        )
-    
+        namespace="mlflow",
+        kubernetes_credentials=kubernetes_credentials,
+    )
+
     for result in results:
         
+        logger.info(f"Processing pod for {result.workflow_model_name}")
+
         pod, chksum = workflow_to_pod(result)
 
-        already_exists,_ = check_if_model_already_exists(namespaced_pod_list, chksum)
+        already_exists, same_version, old_pod = check_if_model_already_exists(
+            namespaced_pod_list,
+            result.workflow_model_name,
+            result.workflow_model_version,
+        )
 
-        logger.info(f"Checking if pod {result.workflow_model_name} already exists in list {namespaced_pod_list}")
-        
-        if not already_exists:
+        if not already_exists and not same_version:
+            logger.info(
+                f"Creating pod {result.workflow_model_name} with version {result.workflow_model_version}"
+            )
             create_namespaced_pod(
                 namespace="mlflow",
                 new_pod=pod,
                 kubernetes_credentials=kubernetes_credentials,
             )
+            
+            set_registered_model_tag(
+                result.workflow_model_name,
+                "deployment_status",
+                "deployment_running",
+            )
+            set_registered_model_tag(
+                result.workflow_model_name,
+                "deployment_pod_name",
+                pod.metadata["name"] # its annoying this is different
+            )
 
+        elif already_exists and not same_version:
+            logger.info(f"Deleting pod {result.workflow_model_name}")
+            delete_namespaced_pod(
+                kubernetes_credentials=kubernetes_credentials,
+                pod_name=old_pod.metadata.name,
+                namespace="mlflow",
+            )
+            set_registered_model_tag(
+                result.workflow_model_name,
+                "deployment_status",
+                "updating_deployment",
+            )
+            
+            create_namespaced_pod(
+                namespace="mlflow",
+                new_pod=pod,
+                kubernetes_credentials=kubernetes_credentials,
+            )
+            set_registered_model_tag(
+                result.workflow_model_name,
+                "deployment_status",
+                "deployment_running",
+            )
+            set_registered_model_tag(
+                result.workflow_model_name,
+                "deployment_pod_name",
+                pod.metadata["name"] # its annoying this is different
+            )
+        
+
+        else:
+            logger.info(
+                f"Pod {result.workflow_model_name} with version {result.workflow_model_version} already exists"
+            )
+
+    # also handle hanging pods that are not in the list
 
 
 if __name__ == "__main__":
